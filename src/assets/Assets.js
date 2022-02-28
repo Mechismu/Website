d76fb6577f3e15d3ef699a72309954d844203042
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2022-02-28 22:46:11 +0530

    Logo Added to Assets

--- src/assets/Assets.js
+++ src/assets/Assets.js
@@ -2,17 +2,36 @@ import logo from './logo.png';
 import heroBanner from './heroBanner.jpeg';
 import ansyslogo from './Sponsors/ansyslogo.jpg';
 import aranyalogo from './Sponsors/aranyalogo.jpeg';
-import autodesklogo from './Sponsors/Autodesk-Logo.jpg';
+
 import Burnout from './Sponsors/Burnout.png';
 import fmsci from './Sponsors/fmsci.jpg';
 import gail from './Sponsors/gail.png';
 import ims from './Sponsors/ims.png';
 import pioneer from './Sponsors/pioneer.png';
 import sbilogo from './Sponsors/sbi-logo.png';
+import seo from './Sponsors/seo.jpg';
+import ricardo from './Sponsors/ricardo';
+import mathworks from './Sponsors/mathworks';
+import jktyre from './Sponsors/jktyre';
+import autodesk from './Sponsors/autodesk.jpg'
+
 
 const ASSETS = {
   logo,
   heroBanner,
+  ansyslogo,
+  aranyalogo,
+  Burnout,
+  fmsci,
+  gail,
+  ims,
+  pioneer,
+  sbilogo,
+  seo,
+  ricardo,
+  mathworks,
+  jktyre,
+  autodesk
 };
 
 export default ASSETS
