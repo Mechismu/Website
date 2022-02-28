8eb12de5a3f5d9aa9b4a34c75a943cacfec93881
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2022-03-01 04:19:57 +0530

    sponorsname

--- src/screens/OurSponsors.js
+++ src/screens/OurSponsors.js
@@ -10,79 +10,79 @@ export default function OurSponsors() {
       <div className="container">
   <img src={ASSETS.ansyslogo} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">Ansys</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.aranyalogo} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">Aranya</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.autodesk} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">autodesk</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.fmsci} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">fmsci</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.gail} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">Gail</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.jktyre} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">J K Tyre</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.ims} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">Ims</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.mathworks} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">mathworks</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.pioneer} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">Pioneer</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.ricardo} alt="Avatar" className='image' />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">Ricardo</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.sbilogo} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">SBI</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.seo} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">SEO</div>
   </div>
 </div>
 <div className="container">
   <img src={ASSETS.Burnout} alt="Avatar" className="image" />
   <div className="overlay">
-    <div className="text">John Doe</div>
+    <div className="text">Burnout</div>
   </div>
 </div>
 
