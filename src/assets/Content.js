f090dfdfd68c517c95e0baf4a89d64eceab73841
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2022-02-28 23:56:08 +0530

    TeamGallery update

---
+++ src/assets/Content.js
@@ -0,0 +1,161 @@
+const batch2023=[
+    {
+    name: "Rahul Gupta",
+    department: "Steering and Suspension",
+    email : "rg7808329@gmail.com",
+    linkdin:""
+    },
+    {
+      name : "Rajat Rai",
+      department : "",
+      email:"",
+      linkdin:""
+
+    },{
+        name :"P.Govind Rao",
+        department:"Powertrain",
+        email:"",
+        linkdin:""
+    },{
+        name : "Anshuman Tejaswani",
+        department:"Powertrain",
+        email:"",
+        linkdin:""
+    },
+    {
+        name :"Gunjan Haldar",
+        department:"Steering and Suspension",
+        email:"",
+        linkdin:""
+    },{
+        name:"Suman Ranjan",
+        department:"Operations and Mangement",
+        email:"",
+        linkdin:""
+    },{
+        name :"Aditya Gautam",
+        department:"Brakes",
+        email:"",
+        linkdin:""
+    },{
+        name:"Kakarla Gowtham",
+        department:"Brakes",
+        email:"",
+        linkdin:""
+    },{
+        name:"Bitan Duary",
+        department:"Transmission",
+        email:"",
+        linkdin:""
+    },{
+        name:"Sai Vivek Reddy Mettu",
+        department:"Brakes",
+        email:"",
+        linkdin:""
+    }
+
+];
+const batch2024=[
+    {
+        name:"Divyansh Bansal",
+        department:"Steering and Suspension",
+        email:"",
+        linkdin:""
+    },{
+        name:"Kalluri Spoorthy",
+        department:"Powertrain",
+        email:"",
+        linkdin:""
+    },{
+        name:"Tanisha Chandra",
+        department:"Chasis",
+        email:"",
+        linkdin:""
+    },{
+        name:"Aniket Sahu",
+        department:"Steering and Suspension",
+        email:"",
+        linkdin:""
+    },{
+        name:"Mudit",
+        department:"Steering and Suspension",
+        email:"",
+        linkdin:""
+    },{
+        name:"Dheeraj",
+        department:"Operations and Management",
+        email:"",
+        linkdin:""
+    },{
+        name:"Neeharika",
+        department:"Operations and Management",
+        email:"",
+        linkdin:""
+    }
+];
+const batch2022=[
+    {
+        name:"Akshay Kargaonkar",
+        department:"",
+        email:"ak.18je0074@mech.iitism.ac.in",
+        linkdin:"https://www.linkedin.com/in/akshay-kargaonkar-48505517a"
+    },{
+        name:"Badireddy Sai Ganesh",
+        department:"",
+        email:"",
+        linkdin:"https://www.linkedin.com/in/badireddy-sai-ganesh-84187019b/"
+    },{
+        name:"Intipalli Vinay Venkata Narasimha Swamy",
+        department:"",
+        email:"",
+        linkdin:"https://www.linkedin.com/in/intipalli-vinay-venkata-narasimha-swamy-48846119a/"
+    },{
+        name:"Lakhinana Harshvardhan",
+        department:"Steering and Suspension",
+        email:"",
+        linkdin:"https://www.linkedin.com/in/lakhinana-harshavardhan-b98898186"
+    },{
+        name:"Manmohan Labh",
+        department:"",
+        email:"",
+        linkdin:""
+    },{
+        
+            name:"Prateek Kesarwani",
+            department:"Steering and Suspension",
+            email:"",
+            linkdin:"https://www.linkedin.com/in/prateek-kesarwani-9a136616a"
+        
+    },{
+        name:"Sagar Rawat",
+            department:"",
+            email:"",
+            linkdin:"https://www.linkedin.com/in/sagar-rawat-60a22b1b2"
+        
+    },{
+        name:"Sanjay Kumar Sandineni",
+            department:"",
+            email:"",
+            linkdin:"https://www.linkedin.com/in/sanjay-kumar-sandineni-b72296170"
+        
+    },{
+        name:"Sankalp",
+            department:"",
+            email:"",
+            linkdin:"https://www.linkedin.c
+            om/in/sankalp-945792187
+            "
+        
+    },{
+        name:"Sudeepto Paul",
+            department:"",
+            email:"",
+            linkdin:"https://www.linkedin.com/in/sudeepto-paul-31418a199"
+    },{
+        name:"Suman Shekhar",
+        department:"",
+        email:"",
+        linkdin:"https://www.linkedin.com/in/suman-shekhar-21290816a"
+
+    }
+]
