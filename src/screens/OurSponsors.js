693cb75808ca6b82359858a1d13ee89639670e7b
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2022-03-01 04:12:41 +0530

    SponsorsUpdated

--- src/screens/OurSponsors.js
+++ src/screens/OurSponsors.js
@@ -1,17 +1,93 @@
 import React from 'react';
 import './OurSponsors.css';
-import ansyslogo from '../assets/Sponsors/ansyslogo';
+import ASSETS from '../assets/Assets';
+
 
 export default function OurSponsors() {
   return (
     <div>
       <h1>Our Sponsors</h1>
-      <div>
-        <img src={ansyslogo} ></img>
-       
-      </div>
+      <div className="container">
+  <img src={ASSETS.ansyslogo} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.aranyalogo} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.autodesk} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.fmsci} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.gail} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.jktyre} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.ims} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.mathworks} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.pioneer} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.ricardo} alt="Avatar" className='image' />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.sbilogo} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.seo} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
+<div className="container">
+  <img src={ASSETS.Burnout} alt="Avatar" className="image" />
+  <div className="overlay">
+    <div className="text">John Doe</div>
+  </div>
+</div>
 
+
+</div>
       
-    </div>
-  )
+  );
 }
