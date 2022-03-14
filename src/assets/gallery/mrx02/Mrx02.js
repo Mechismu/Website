e7ac569e5b7fb1a0aa552f6a0bbd1002dc926268
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2022-03-14 17:29:50 +0530

    mrx02picsadded

--- src/assets/gallery/mrx02/Mrx02.js
+++ src/assets/gallery/mrx02/Mrx02.js
@@ -1,5 +1,9 @@
-import mrx02 from "./mrx02.jpeg";
-import mrx02specs from "./mrx02specs.jpeg";
+import mrx02 from "./mrx02.jpeg"
+import mrx02specs from "./mrx02specs.jpeg"
+import first from "./01.png";
+import second from "./02.png";
+import third from "./03.png"
+
 
 const MRX02 = {
   name: "MRX02",
@@ -7,7 +11,7 @@ const MRX02 = {
   image: mrx02,
   details:"THE ONLY REAL MISTAKE IS THE ONE FROM WHICH WE LEARN NOTHING -Henry Ford.We as a team believe in this very well said quote we improve with every car we make MRX-02 is our another achievement of which we are proud ofAnd what to talk about its specification it has inline single cylinder 4 stroke engine with water-cooled DOHC , having max power 22.1HP at 9300 rpm and a top speed of 130km/hrIts chassis was also one of a kind having clearance a clearance of 71.54 in front and 60.87mm in rearWith a wheelbase of 1574.8mm and max height of 1092.2mmThe brakes are having byber hydraulic callipers and the front-outboard has 220mm and the rear-outboard having 220 mm Suspensions consist of unequal length non-parallel double-wishbone coils springs and hydraulic dampers and also anti-roll bar assist in front",
   specs: mrx02specs,
-  gallery: [mrx02, mrx02, mrx02],
+  gallery: [mrx02, first, second,third],
 };
 
 export default MRX02;
