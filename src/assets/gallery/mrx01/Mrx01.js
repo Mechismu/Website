7c2b666113c1492fabcb0cf8b641804dcaa11b10
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2022-03-14 17:33:42 +0530

    mrx01picsadded

--- src/assets/gallery/mrx01/Mrx01.js
+++ src/assets/gallery/mrx01/Mrx01.js
@@ -1,5 +1,7 @@
 import mrx01 from "./mrx01.jpeg";
 import mrx01specs from "./mrx01specs.jpeg";
+import emrax from "./emrax.png";
+import emrax1 from "./emrax-1.png"
 
 const MRX01 = {
   name: "MRX01",
@@ -7,7 +9,7 @@ const MRX01 = {
   image: mrx01,
   details: "“THE SECRET OF SUCCESS IS TO DO COMMON THING UNCOMMONLY WELL.A very well said line by John . D. Rockfeller that inspires us to work harder.We as a team have already built many cars one of them is MRX it was one of the most sought after cars and we as a team take pride that we built such a robust masterpiece And what to talk about its specification it has inline single cylinder 4 stroke engine with water-cooled DOHC , having max power 22.1HP at 9300 rpm and a top speed of 130km/hrIts chassis was also one of a kind having clearance a clearance of 71.54 in front and 60.87mm in rearWith wheelbase of 1574.8mm and max height of 1092.2mmThe brakes are having byber hydraulic callipers and the front-outboard has 220mm and the rear-outboard having 220 mm Suspensions consist of unequal length non-parallel double-wishbone coils springs and hydraulic dampers and also anti-roll bar assist in front",
   specs: mrx01specs,
-  gallery: [mrx01, mrx01, mrx01],
+  gallery: [mrx01, emrax, emrax1],
 };
 
 export default MRX01;
